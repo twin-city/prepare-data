c295ddb48dfbec3f4cd4e5af045e2882f90951e1
tsolus <tristan.solus@gmail.com>
2022-06-13 15:35:40 +0000

    :foot:

--- prepare_data/tree.py
+++ prepare_data/tree.py
@@ -1,14 +1,49 @@
 import requests
 import pandas as pd
 import json
+import requests
+from pathlib import Path
+import geopandas as gpd
+import geojson
+import shapely
+from shapely.geometry import Polygon, LineString, MultiLineString, MultiPolygon, collection, Point
 
 def get(url):
-    import pdb; pdb.set_trace()
-    print(url)
-    return url
+    data_tree = requests.get(url)
+    if data_tree.status_code == 200:
+        return data_tree.json()
+    else:
+        return data_tree.status_code
 
-def prepare():
-    pass
+def save(data, path:Path):
+    data_arbres = json.dumps(data)
+    with open(path, 'w') as outfile:
+        outfile.write(data_arbres)
 
-def save():
-    pass
+def prepare(data):
+    data = pd.DataFrame.from_dict(data)
+    arbres = pd.json_normalize(data['fields'])
+    arbres = arbres[['hauteurenm', 'libellefrancais', 'geo_point_2d']]
+    arbres.rename(columns={'hauteurenm':'hauteur', 'libellefrancais':'espece'})
+
+    quartier = Polygon([(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)])
+    coordonnees=[]
+    index_to_remove=[]
+    repere = ['x','y']
+    for i, row in arbres.iterrows():
+        coordonnees.append(row['geo_point_2d'][::-1])
+        point = Point(row['geo_point_2d'][1], row['geo_point_2d'][0])
+        if not quartier.contains(point):
+            index_to_remove.append(i)
+    
+    for i in range(len(coordonnees)):
+        coordonnees[i] = dict(zip(repere, coordonnees[i]))
+
+    arbres['coordonnees']=coordonnees
+    arbres.drop('geo_point_2d', axis='columns', inplace=True)
+    arbres.drop(axis=0, index = index_to_remove, inplace = True)
+    data_arbres = pd.DataFrame(arbres)
+    return data_arbres
+
+def write(data):
+    data.to_json('/workspace/home/data/arbres_place_st_augustin.json', orient = "table")
--- tests/test_trees.py
+++ tests/test_trees.py
@@ -1,7 +1,19 @@
-from prepare_data.tree import get, prepare, save
+from tree import get, prepare, save, write
+from pathlib import Path
 
+url ='https://opendata.paris.fr/explore/dataset/les-arbres/download/?format=json&timezone=Europe/Berlin&lang=fr&epsg=2154'
+path = '/workspace/home/data/arbres_paris.json'
+ 
 def test_get():
-    url = ""
-    #import pdb; pdb.set_trace()
     data_tree = get(url)
-    assert data_tree == ''
+    assert type(data_tree) == list
+
+def test_save():
+    save(get(url), path)
+
+def test_prepare():
+    data_arbres = prepare(get(url))
+    assert len(data_arbres)== 122
+
+def test_write():
+    write(prepare(get(url)))
