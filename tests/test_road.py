ecca54c3677d3116aad5a62a4e3a47aedfc27e80
victorjourne <journe.victor@gmail.com>
2022-06-14 15:16:31 +0200

    Revert ":construction: work in progress"
    This reverts commit a17e68d8fd25675b8ba6499a8841983fff061391.

--- tests/test_road.py
+++
@@ -1,7 +0,0 @@
-from road import get
-from pathlib import Path
-
-url = 'https://wxs.ign.fr/859x8t863h6a09o9o6fy4v60/telechargement/prepackage/BDTOPOV3-TOUSTHEMES-DEPARTEMENT_GPKG_PACK_221$BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D075_2022-03-15/file/BDTOPO_3-0_TOUSTHEMES_GPKG_LAMB93_D075_2022-03-15.7z'
-
-def test_get():
-    get(url)
--- tests/test_trees.py
+++ tests/test_trees.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 url ='https://opendata.paris.fr/explore/dataset/les-arbres/download/?format=json&timezone=Europe/Berlin&lang=fr&epsg=2154'
 path = '/workspace/home/data/arbres_paris.json'
-quartier = Polygon([(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)]) 
-
+ 
 def test_get():
     data_tree = get(url)
     assert type(data_tree) == list
@@ -13,7 +12,7 @@ def test_save():
     save(get(url), path)
 
 def test_prepare():
-    data_arbres = prepare(get(url), quartier)
+    data_arbres = prepare(get(url))
     assert len(data_arbres)== 122
 
 def test_write():
