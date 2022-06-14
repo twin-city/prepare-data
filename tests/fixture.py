c410cb69cc1f95a4d361ff3e9445ce2b3d74f088
victorjourne <journe.victor@gmail.com>
2022-06-14 18:31:13 +0200

    :sparkles: Tree json is build

--- tests/fixture.py
+++ tests/fixture.py
@@ -1,12 +1,12 @@
 import pytest
 import geopandas as gpd
-from geojson import Polygon
-
-CRS = "EPSG:2154"#lambert
-saint_augustin = [[(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)]]
+#from geojson import Polygon
+from shapely.geometry import Polygon
+#CRS = "EPSG:2154"#lambert
+saint_augustin = [(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)]
 
 @pytest.fixture
 def quartier():
-    quartier=gpd.GeoSeries([Polygon(saint_augustin)])
-    quartier =  quartier.set_crs(CRS) # lambert
-    return quartier
+    #quartier = gpd.GeoSeries([Polygon(saint_augustin)])
+    #quartier =  quartier.set_crs(CRS) # lambert
+    return  Polygon(saint_augustin)
