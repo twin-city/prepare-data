0224cce107ee712714a8c19deb928dfcaa54130a
tsolus <tristan.solus@gmail.com>
2022-06-21 08:15:45 +0000

    :construction: tests passed

--- prepare_data/road.py
+++ prepare_data/road.py
@@ -20,14 +20,13 @@ def get(url, data_path):
     file_name = 'bd_topo_75.7z'
     with (data_path / file_name).open('wb') as f:
         f.write(req.content)
+    return data_path
+
+def prepare(quartier, data_path):
     with py7zr.SevenZipFile(str(data_path /'bd_topo_75.7z'), 'r') as archive:
         allfiles = archive.getnames()
         selective_files = [f for f in allfiles if filter_pattern.match(f)]
-        print(selective_files)
         archive.extract(targets=selective_files,path=str(data_path))
-    return data_path
-
-def prepare(df, quartier):
     df = gpd.read_file(data_path / selective_files[0], mask=quartier, crs = CRS, layer='troncon_de_route')
     df = df[['nom_1_gauche', 'largeur_de_chaussee', 'sens_de_circulation', 'geometry']]
     coordonnees =[]
@@ -41,7 +40,7 @@ def prepare(df, quartier):
         new_line.append(new_point2)
         coordonnees.append(new_line)
     df['coordonnees'] = coordonnees
-    import pdb; pdb.set_trace()
+    df = pd.DataFrame(df)
     df.drop('geometry', axis='columns', inplace=True)
     return df
 
