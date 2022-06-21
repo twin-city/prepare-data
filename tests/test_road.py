1e48a3f757f184d0d060d28b17a2eee57ba8a011
victorjourne <journe.victor@gmail.com>
2022-06-21 15:36:17 +0200

    Update test_road.py

--- tests/test_road.py
+++ tests/test_road.py
@@ -22,5 +22,4 @@ def test_write(quartier):
     path_road = data_path / 'road.json'
     data_road = prepare(_path_gpkg, quartier)
     write(path_road, data_road)
-    import pdb ; pdb.set_trace()
     assert path_road.stat().st_size == 4671
