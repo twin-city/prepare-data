6acae9b567daa995765eca7478b6996b7ee55552
victorjourne <journe.victor@gmail.com>
2022-06-21 12:13:26 +0200

    :boom: Test tree passend

--- prepare_data/tree.py
+++ prepare_data/tree.py
@@ -16,21 +16,22 @@ CRS = "EPSG:2154"#lambert
 inProj  = Proj(f"+init={CRS}",preserve_units=True)
 outProj = Proj("+init=EPSG:4326") # WGS84 in degrees and not EPSG:3857 in meters)
 # swap x,y as mkennedy says
-
 data_path = Path(os.getenv('DATA_PATH'))
-
 
 def get(url, quartier, force: bool=False) -> Path:
 
     path_json = data_path / 'les-arbres.json'
-
     dataset = 'les-arbres'
-    quartier = [(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)]
-
+    columns = ['libellefrancais','genre', 'espece', 'varieteoucultivar','circonferenceencm','hauteurenmf']
+    rows = 1000
+    #quartier = [(649985, 6864006), (650266, 6864006),(650266, 6864226), (649985, 6864226),  (649985, 6864006)]
+    quartier = [(x,y) for x,y in zip(*quartier.exterior.coords.xy)]
     polygon = ','.join([str(transform(inProj, outProj, x, y)[::-1]) for x,y in quartier])
-    list_facet = '&facet=libellefrancais&facet=genre&facet=espece&facet=varieteoucultivar&facet=circonferenceencm&facet=hauteurenmf'
-
-    url = url.format(dataset=dataset, polygon=polygon, list_facet=list_facet)
+    list_facet = '&'.join(['facet=' + col for col in columns])
+    url = url.format(dataset=dataset,
+                    polygon=polygon,
+                    list_facet=list_facet,
+                    rows=rows)
 
     if force or (not path_json.exists()):
         data_tree = requests.get(url)
@@ -44,12 +45,10 @@ def get(url, quartier, force: bool=False) -> Path:
         data = load(path_json)
         return data
 
-def prepare(data_json: list, quartier):
+def prepare(data_json: list):
     #data = load(path_json)
-
-    data = pd.DataFrame.from_dict(data_json)
+    data = pd.DataFrame.from_dict(data_json['records'])
     arbres = pd.json_normalize(data['fields'])
-    import pdb; pdb.set_trace()
     arbres = arbres[['hauteurenm', 'libellefrancais', 'geo_point_2d']]
     arbres.rename(columns={'hauteurenm':'hauteur', 'libellefrancais':'espece'})
 
@@ -58,9 +57,6 @@ def prepare(data_json: list, quartier):
     repere = ['x','y']
     for i, row in arbres.iterrows():
         coordonnees.append(row['geo_point_2d'][::-1])
-        point = Point(row['geo_point_2d'][1], row['geo_point_2d'][0])
-        if not quartier.contains(point):
-            index_to_remove.append(i)
 
     for i in range(len(coordonnees)):
         coordonnees[i] = dict(zip(repere, coordonnees[i]))
@@ -69,8 +65,8 @@ def prepare(data_json: list, quartier):
     arbres.drop('geo_point_2d', axis='columns', inplace=True)
     arbres.drop(axis=0, index = index_to_remove, inplace = True)
     data_arbres = pd.DataFrame(arbres)
-    return data_arbres.to_dict()
+    return data_arbres.to_dict(orient='records')
 
 def write(path_tree: Path, data, force=True):
     if force or (not path_tree.exists()):
-        data.to_json(path_tree, orient = "table")
+        write_json(path_tree, data)
