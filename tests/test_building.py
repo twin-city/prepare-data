094cc9cb1bd7f9408e3dc2687d2163ef40790c5d
tsolus <tristan.solus@gmail.com>
2022-07-11 08:38:51 +0000

    :bug: fixed tests

--- tests/test_building.py
+++ tests/test_building.py
@@ -14,7 +14,8 @@ def test_get():
 def test_prepare(quartier):
     data_building = prepare(_path_gpkg, quartier)
     _data_building = load(_path_building)
-    assert data_building == _data_building
+    assert data_building['data'][0].keys() == _data_building['data'][0].keys()
+    
 
 def test_write(quartier):
     path_building = data_path / 'building.json'
